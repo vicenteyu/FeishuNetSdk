6b5bdd6742e5566b1f6bbc86ff6d2c2a2e03e061
Vicente.Yu <^@^>
2024-06-12 02:57:57 +0800

    响应结果增加错误详细信息
    Signed-off-by: Vicente.Yu <^@^>

--- src/FeishuResponse.cs
+++ src/FeishuResponse.cs
@@ -29,6 +29,33 @@ public record FeishuResponse<T>
     /// </summary>
     [JsonIgnore]
     public bool IsSuccess => Code == 0;
+
+    /// <summary>
+    /// 错误信息
+    /// </summary>
+    [JsonPropertyName("error")]
+    public ErrorSuffix? Error { get; set; }
+
+    /// <summary>
+    /// 错误信息
+    /// </summary>
+    /// <param name="Message">错误消息</param>
+    /// <param name="LogId">Log Id</param>
+    /// <param name="FieldViolations">违规字段</param>
+    public record ErrorSuffix([property: JsonPropertyName("message")] string? Message,
+        [property: JsonPropertyName("log_id")] string? LogId,
+        [property: JsonPropertyName("field_violations")] ErrorSuffix.FieldViolation[]? FieldViolations)
+    {
+        /// <summary>
+        /// 违规字段
+        /// </summary>
+        /// <param name="Field">字段名</param>
+        /// <param name="Value">字段值</param>
+        /// <param name="Description">描述</param>
+        public record FieldViolation([property: JsonPropertyName("field")] string Field,
+            [property: JsonPropertyName("value")] string Value,
+            [property: JsonPropertyName("description")] string Description);
+    }
 }
 
 /// <summary>
